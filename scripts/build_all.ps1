e05e7ddc3917046d0e57997a41de6d1e4d4bf9b3
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2025-07-09 19:05:17 -0400

    Update build_all.ps1

--- scripts/build_all.ps1
+++ scripts/build_all.ps1
@@ -1,12 +1,10 @@
 & "$PSScriptRoot\..\gitmodules\mod-installer\build_all.ps1" `
-  -Version "<version>" `
+  -Version "2.0.0" `
   -PreReleaseVersion "beta.1" `
-  -OutputFilePrefix "<output file prefix>" `
-  -ModName "<mod name>" `
-  -ModFolder "<mod folder>" `
-  -AdditionalModFolders @() `
+  -OutputFilePrefix "Awakening_1.1" `
+  -ModName "Awakening 1.1" `
+  -ModFolder "Awakening" `
   -ModFilesPath "$PSScriptRoot\..\game" `
-  -ModBaseFilesUrl "<url to mod base files zip>" `
-  -ModBaseFilesUrlHash "<mod base files zip sha256>" `
-  -ModReadmePath "<mod readme path>" `
+  -ModBaseFilesUrl "https://github.com/HL2-Mods-Legacy-Project/hl2-legacy-registry/releases/download/awakening/Awakening-1.1-Original.zip" `
+  -ModBaseFilesUrlHash "45875752ce89f74271bb09cf644708d996a877272086ce249133428ef77507c6" `
   -PublishPath "$PSScriptRoot\..\artifacts\publish"
